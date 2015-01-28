63025e234cc97119dda425e78dc25f678b5a7f4a
brynjarte <brynjarte@gmail.com>
2015-01-28 12:28:20 +0100

    Create UDP.go

---
+++ Ex3/UDP.go
@@ -0,0 +1,46 @@
+package main
+
+import (
+	"fmt"
+	"time"
+	"net"
+
+)
+
+
+
+
+func receive_UDP() (string){
+
+	addr, _ := net.ResolveUDPAddr("udp",":30000")
+	sock, _ := net.ListenUDP("udp",addr)
+	buffer := make([]byte, 1024)
+	rlen,_,_ := sock.ReadFromUDP(buffer)
+	
+	return string(buffer[0:rlen])
+	
+}
+
+func send_UDP(){
+	
+	
+	addr, _ := net.ResolveUDPAddr("udp","129.241.187.255:20005")
+	con,_ := net.DialUDP("udp",nil,addr)
+	buf := []byte("JAJA")
+	time.Sleep(100*time.Millisecond)
+	_,err := con.Write(buf)
+
+	if err != nil {
+		fmt.Println(err)
+	}
+	
+}
+
+
+func main (){
+	
+	send_UDP()
+	fmt.Println(receive_UDP())
+
+
+}
